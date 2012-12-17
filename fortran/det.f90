496cc740f1be429b07698c72adc020d4c1145869
Ashwith Jerome Rego <ashwith@ashwith.home>
2012-12-18 01:13:42 +0530

    Added GPL 3.0 license

--- fortran/det.f90
+++ fortran/det.f90
@@ -5,7 +5,23 @@
 !row operations (column operations would work as well) are used
 !to convert the matrix into upper traingular form
 !2]The determinant of a triangular matrix is obtained by finding the product of the diagonal elements
+
+!Copyright (C) 2007 Ashwith Jerome Rego
 !
+!This program is free software: you can redistribute it and/or modify
+!it under the terms of the GNU General Public License as published by
+!the Free Software Foundation, either version 3 of the License, or
+!(at your option) any later version.
+!
+!This program is distributed in the hope that it will be useful,
+!but WITHOUT ANY WARRANTY; without even the implied warranty of
+!MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!GNU General Public License for more details.
+!
+!You should have received a copy of the GNU General Public License
+!along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 REAL FUNCTION FindDet(matrix, n)
   IMPLICIT NONE
   REAL, DIMENSION(n,n) :: matrix
