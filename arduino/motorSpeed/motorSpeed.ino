637713bc05e7093506375f6cdcb7d79cca6eead8
ashwith <ashwith@gmail.com>
2014-03-06 21:51:37 +0530

    Initial Version

--- arduino/motorSpeed/motorSpeed.ino
+++ arduino/motorSpeed/motorSpeed.ino
@@ -1,3 +1,27 @@
+//Copyright (C) 2014 Ashwith Jerome Rego
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+//Calculates the speed of a motor.
+//Hardware is a IR sensor which 
+//faces a wheel with 8 holes.
+//Speed is calculated by measuring
+//time between consecutive holes.
+//A 3-point average is used to 
+//smooth out any errors.
+
 int pwmPin = 3;     //Motor speed control
 int encPin = 2;     //Input for encoder pulse
 int speedPin = A0;  //Motor speed knob
